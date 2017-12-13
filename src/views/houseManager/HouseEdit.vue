922f26c5e5f9df89bf7d4e8b7f92256708b5fffd
wangqichao <wangqichao@hdsc.com>
2017-12-13 15:42:06 +0800

    test11

--- src/views/houseManager/HouseEdit.vue
+++ src/views/houseManager/HouseEdit.vue
@@ -61,7 +61,7 @@ export default {
             rules: { // Form表单字段验证规则
                 name: [
                     { required: true, message: '请输入组织名称', trigger: 'blur' },
-                    { min: 1, max: 30, message: '长度在 1 到 30 个字符', trigger: 'blur' }
+                    { min: 1, max: 33, message: '长度在 1 到 30 个字符', trigger: 'blur' }
                 ],
                 memo: [
                     { min: 1, max: 100, message: '长度在 1 到 100 个字符', trigger: 'blur' }
